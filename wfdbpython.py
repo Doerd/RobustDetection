e93cb5d60e2a4d122007332c7442e00fc576a3b5
williamadriance <williamadriance>
2016-12-14 12:53:13 -0800

    yay

--- wfdbpython.py
+++ wfdbpython.py
@@ -198,8 +198,9 @@ N = 600
 # sample spacing
 T = 1.0 / 800.0
 x = np.linspace(0.0, N*T, N)
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
+y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(200.0 * 2.0*np.pi*x)
 yf = scipy.fftpack.fft(y)
+print(yf)
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
 fig, ax = pyplot.subplots()
